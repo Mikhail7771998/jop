07f48f6e7b746d6b7d149924097366560b14043b
Mikhail7771998 <vivismvi@yandex.ru>
2023-07-27 14:31:22 +0300

    li

---
+++ key.py
@@ -0,0 +1,16 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+url = 'https://www.kiddle.co/s.php?q=cats'
+
+page = requests.get(url).text
+soup = BeautifulSoup(page, 'html.parser')
+
+for raw_img in soup.find_all('img'):
+    link = raw_img.get('src')
+    if link and link.startswith('https'):
+        response = requests.get(link)
+        with open("today_avatar.jpg", 'wb') as f:
+            f.write(response.content)
+        break
